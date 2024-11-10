2574fbce9ff12a534f14d14464f2a418b7c10fd2
Himanshu <himanshupanwar007@yahoo.co.in>
2024-11-10 22:50:54 +0530

    interest amount calculator

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,40 @@
+import { Component, computed, inject, input, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  // @Input() results?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+
+  private investmentService = inject(InvestmentService);
+
+  //using signals
+  // results = input<{
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[]>()
+
+  // get results() {
+  //   return this.investmentService.resultData;
+  // }
+
+  //using signals
+  results = computed(() => this.investmentService.resultData());
+}
